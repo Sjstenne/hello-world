a3bc44f8db977b4b8e129add596475da8537a32c
Sjstenne <79822538+Sjstenne@users.noreply.github.com>
2021-02-28 22:31:18 -0700

    reverseNumber() remove comment
    Removed a comment from reverseNumber() state that I changed the data type, when I kept it the same.

--- GitDemo.java
+++ GitDemo.java
@@ -23,7 +23,7 @@ public class GitDemo {
      *  the function will return the value of (1 * 2 * 3 * 4)
      *****/
     public double findFactorial(int num) {
-        double factorialNumber = 1; // change data type to int since factorials can only ever return integers
+        double factorialNumber = 1;
         
         for(int i = 1; i <= num; ++i){
             factorialNumber = factorialNumber * i;
